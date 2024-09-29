24f7830b27109a2853a9ad2a88817554b7d11eef
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2024-09-29 10:49:51 +0500

    Add files via upload

---
+++ Assignment-2/app/components/header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+<header>
+    <h1>
+      Welcome to nextjs
+    </h1>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a> <br />
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+    )
+}
---
+++ Assignment-2/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "./components/header";
+
+import About from "./components/about"
+
+export default function Home() {
+  return (
+  <div>
+    <Header/>
+    <About/>
+  </div>
+  );
+}
