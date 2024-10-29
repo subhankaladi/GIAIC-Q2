193da69525311cb41e7027429a25e100ae5e8e5c
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2024-10-30 03:24:53 +0500

    Update page.tsx

--- assingnment-3-routing/app/about/page.tsx
+++ assingnment-3-routing/app/about/page.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 const About = () => {
   return (
     <div><header>
 
 
-    <ul>
-      <li><a href="/home">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
+<ul>
+          <li><Link href="/home">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
   </header>
   <h1>About Page</h1>
   </div>
