4bf420576e8dd037ad29f9aa5903b489bc38709c
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2024-11-10 14:05:41 +0500

    Update about.tsx

--- Assignment-3/app/components/about.tsx
+++ Assignment-3/app/components/about.tsx
@@ -1,7 +1,7 @@
 export default function About(){
     return(
         <div>
-            <a href="./main">Back to home Page</a>
+            <a href="./main">Back to home Page.</a>
         </div>
     )
 }
